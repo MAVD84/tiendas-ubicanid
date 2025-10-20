d8f20e65e64c8dd3dbd622d7d3fad5e5b178564e
mavd09 <mavd09@gmail.com>
2025-10-20 22:02:16 +0000

    Apply light theme and update app title

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../providers/point_of_sale_provider.dart';
-import '../providers/theme_provider.dart';
 import '../widgets/point_of_sale_list.dart';
 import '../widgets/search_bar.dart';
 import 'add_edit_screen.dart';
@@ -22,23 +21,9 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final themeProvider = Provider.of<ThemeProvider>(context);
-
     return Scaffold(
       appBar: AppBar(
-        title: const Text('UbicanID Tiendas'),
-        actions: [
-          IconButton(
-            icon: Icon(themeProvider.themeMode == ThemeMode.dark ? Icons.light_mode : Icons.dark_mode),
-            onPressed: () => themeProvider.toggleTheme(),
-            tooltip: 'Toggle Theme',
-          ),
-          IconButton(
-            icon: const Icon(Icons.auto_mode),
-            onPressed: () => themeProvider.setSystemTheme(),
-            tooltip: 'Set System Theme',
-          ),
-        ],
+        title: const Text('Ubican ID Tiendas'),
       ),
       body: Column(
         children: [
